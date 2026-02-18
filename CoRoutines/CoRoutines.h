0a208e75dd4c959d14718c03bce9baeb942437ae
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2026-02-18 23:40:13 +0300

    CoRoutines: Refactored and hardened

--- CoRoutines/CoRoutines.h
+++ CoRoutines/CoRoutines.h
@@ -8,93 +8,173 @@
 #include <type_traits>
 
 namespace Upp {
-    
+
 using CoSuspend     = std::suspend_always;
 using CoDontSuspend = std::suspend_never;
-
 
 enum class CoRoutineType { Routine, Generator };
 
 template<CoRoutineType R, typename T>
 class CoRoutineT {
+	static constexpr const char *errmsg = "Generator exhausted";
+public:
 
-    template<class C, typename U>
-    struct PromiseType
-    {
-        C get_return_object()                           { return C(C::Handle::from_promise(static_cast<U&>(*this))); }
-        std::suspend_always initial_suspend()           { return {}; }
-        std::suspend_always final_suspend() noexcept    { return {}; }
-        void unhandled_exception()                      { exc = std::current_exception(); }
-        std::exception_ptr exc;
-    };
-    
+    // Routine (non-void)
     template<typename U>
-    struct ReturnValue : PromiseType<CoRoutineT, ReturnValue<U>>
-    {
+    struct ReturnValue {
+        using Handle = std::coroutine_handle<ReturnValue>;
+
+        CoRoutineT get_return_object()
+        {
+            return CoRoutineT(Handle::from_promise(*this));
+        }
+
+        std::suspend_always initial_suspend() noexcept { return {}; }
+        std::suspend_always final_suspend()   noexcept { return {}; }
+
         template<std::convertible_to<U> L>
-        void return_value(L&& val) { value = std::forward<L>(val); }
-        void yield_value(U&&) = delete;
+        void return_value(L&& val)
+        {
+            value = std::forward<L>(val);
+        }
+
+        void unhandled_exception()
+        {
+            exc = std::current_exception();
+        }
+
+        std::exception_ptr exc;
         U value {};
     };
 
-    template<typename U>
-    struct VoidValue : PromiseType<CoRoutineT, VoidValue<U>>
-    {
-        void return_void() {}
-    };
-    
-	template<typename U>
-	struct CurrentValue : PromiseType<CoRoutineT, CurrentValue<U>>
-	{
-	    template<std::convertible_to<U> L>
-	    std::suspend_always yield_value(L&& val)
-	    {
-	        value = std::forward<L>(val);
-	        return {};
-	    }
-	    void return_void() noexcept {}   // REQUIRED
-	    std::suspend_never await_transform(U&&) = delete;
-	    void return_value(U&&) = delete;
-	
-	    U value;
-	};
+    // Routine (void)
+    struct VoidValue {
+        using Handle = std::coroutine_handle<VoidValue>;
 
+        CoRoutineT get_return_object()
+        {
+            return CoRoutineT(Handle::from_promise(*this));
+        }
+
+        std::suspend_always initial_suspend() noexcept { return {}; }
+        std::suspend_always final_suspend()   noexcept { return {}; }
+
+        void return_void() noexcept {}
+
+        void unhandled_exception()
+        {
+            exc = std::current_exception();
+        }
+
+        std::exception_ptr exc;
+    };
+
+    // Generator
+    template<typename U>
+    struct CurrentValue {
+        using Handle = std::coroutine_handle<CurrentValue>;
+
+        CoRoutineT get_return_object()
+        {
+            return CoRoutineT(Handle::from_promise(*this));
+        }
+
+        std::suspend_always initial_suspend() noexcept { return {}; }
+        std::suspend_always final_suspend()   noexcept { return {}; }
+
+        template<std::convertible_to<U> L>
+        std::suspend_always yield_value(L&& val)
+        {
+            value = std::forward<L>(val);
+            return {};
+        }
+
+        void return_void() noexcept {}
+
+		template<typename X>
+		std::suspend_never await_transform(X&&) = delete;
+
+
+        void unhandled_exception()
+        {
+            exc = std::current_exception();
+        }
+
+        std::exception_ptr exc;
+        U value {};
+    };
 
 public:
+
     using promise_type =
         typename std::conditional<
             std::is_void_v<T>,
-                VoidValue<T>, typename std::conditional<
+                VoidValue,
+                typename std::conditional<
                     R == CoRoutineType::Routine,
-                        ReturnValue<T>, CurrentValue<T>>::type>::type;
-                        
-    using Handle = typename std::coroutine_handle<promise_type>;
+                        ReturnValue<T>,
+                        CurrentValue<T>
+                >::type
+        >::type;
 
-	bool Do()
-	    requires (R == CoRoutineType::Routine)
-	{
-	    ASSERT(co);
-	    if(co.done())
-	        return false;
-	    co.resume();
-	    Rethrow();
-	    return !co.done();
-	}
-    
-	T Next()
-	    requires (R == CoRoutineType::Generator)
-	{
-	    ASSERT(co);
-	    if(co.done())
-	        throw Exc("Generator exhausted");
-	    co.resume();
-	    Rethrow();
-	    if(co.done())
-	        throw Exc("Generator exhausted");
-	    return co.promise().value;
-	}
+    using Handle = std::coroutine_handle<promise_type>;
+
+    // Routine API
+    bool Do()
+        requires (R == CoRoutineType::Routine)
+    {
+        ASSERT(co);
+
+        if(co.done())
+            return false;
+
+        co.resume();
+        Rethrow();
+
+        return !co.done();
+    }
+
+    T Get() const
+        requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
+    {
+        ASSERT(co);
+        ASSERT(co.done());
+        return co.promise().value;
+    }
 
     T operator~() const
+        requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
+    {
+        return Get();
+    }
+
+    T Pick()
+        requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
+    {
+        ASSERT(co);
+        ASSERT(co.done());
+        return pick(co.promise().value);
+    }
+
+    // Generator API
+    T Next()
+        requires (R == CoRoutineType::Generator)
+    {
+        ASSERT(co);
+
+        if(co.done())
+            throw Exc(errmsg);
+
+        co.resume();
+        Rethrow();
+
+        if(co.done())
+            throw Exc(errmsg);
+
+        return co.promise().value;
+    }
+
+    T operator~()
         requires (R == CoRoutineType::Generator)
     {
         return Next();
@@ -104,38 +184,30 @@ public:
         requires (R == CoRoutineType::Generator)
     {
         ASSERT(co);
+
+        if(co.done())
+            throw Exc(errmsg);
+
         co.resume();
         Rethrow();
+
+        if(co.done())
+            throw Exc(errmsg);
+
         return pick(co.promise().value);
     }
 
-	T Get() const
-	    requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
-	{
-	    ASSERT(co);
-	    ASSERT(co.done());   // must be finished
-	    return co.promise().value;
-	}
+    explicit CoRoutineT(Handle h)
+        : co(h)
+    {}
 
-    T operator~() const
-        requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
+    CoRoutineT(CoRoutineT&& r) noexcept
+        : co(r.co)
     {
-        return Get();
+        r.co = {};
     }
 
-    T Pick()
-       requires (R == CoRoutineType::Routine && !std::is_void_v<T>)
-    {
-        ASSERT(co);
-        return pick(co.promise().value);
-    }
-
-    CoRoutineT(Handle h)
-    : co(h)
-    {
-    }
-    
-    CoRoutineT& operator=(CoRoutineT& r) noexcept
+    CoRoutineT& operator=(CoRoutineT&& r) noexcept
     {
         if(this != &r) {
             if(co)
@@ -146,54 +218,78 @@ public:
         return *this;
     }
 
-    virtual ~CoRoutineT() noexcept
+    CoRoutineT(const CoRoutineT&) = delete;
+    CoRoutineT& operator=(const CoRoutineT&) = delete;
+
+    ~CoRoutineT() noexcept
     {
         if(co)
             co.destroy();
     }
-    
-    CoRoutineT(CoRoutineT&& r) noexcept = default;
-    CoRoutineT(const CoRoutineT&) = delete;
-    CoRoutineT& operator=(const CoRoutineT&) = delete;
 
-
-    // Iterator support for generator type.
-    template<typename C, class U>
-    class IteratorT {
-    public: // C++20 iterator tags...
-        using value_type        = U;
+    // Generator-only
+    class Iterator {
+    public:
+        using value_type        = T;
         using difference_type   = std::ptrdiff_t;
         using iterator_category = std::input_iterator_tag;
-        value_type  operator*() const   { return cogen.co.promise().value;   }
-        value_type  operator->() const  { return cogen.co.promise().value;   }
-        IteratorT& operator++()         { cogen.Rethrow(); cogen.co.resume(); return *this; }
-        IteratorT& operator++(int)      { cogen.Rethrow(); cogen.co.resume(); return *this; }
-        bool operator==(const IteratorT&) const { return false; }
-    
-        IteratorT() = delete;
-        IteratorT(C&&) = delete;
-        IteratorT(C& r) : cogen{r}     {}
-    
+
+        Iterator() = default;
+
+        explicit Iterator(CoRoutineT* g)
+            : gen(g)
+        {
+            if(gen && !gen->co.done()) {
+                gen->co.resume();
+                gen->Rethrow();
+                if(gen->co.done())
+                    gen = nullptr;
+            }
+        }
+
+        value_type operator*() const
+        {
+			ASSERT(gen);
+			return gen->co.promise().value;
+        }
+
+        Iterator& operator++()
+        {
+            gen->co.resume();
+            gen->Rethrow();
+            if(gen->co.done())
+                gen = nullptr;
+            return *this;
+        }
+
+        bool operator==(std::default_sentinel_t) const
+        {
+            return gen == nullptr;
+        }
+
+        bool operator!=(std::default_sentinel_t s) const
+        {
+            return !(*this == s);
+        }
+
     private:
-        C& cogen;
+        CoRoutineT* gen = nullptr;
     };
-    
-    using Iterator = class IteratorT<CoRoutineT<CoRoutineType::Generator, T>, T>;
-    
+
     Iterator begin()
         requires (R == CoRoutineType::Generator)
     {
-        return ++Iterator(*this);
+        return Iterator(this);
     }
 
-    
-    Iterator end()
+    std::default_sentinel_t end()
         requires (R == CoRoutineType::Generator)
     {
-        return Iterator(*this);
+        return {};
     }
 
 private:
+
     void Rethrow() const
     {
         if(co.promise().exc)
@@ -201,13 +297,18 @@ private:
     }
 
 private:
-    Handle co;
+    Handle co {};
 };
+
+// Aliases
 
 template<typename T>
 using CoRoutine = CoRoutineT<CoRoutineType::Routine, T>;
-template<typename T> requires (!std::is_void_v<T>)
+
+template<typename T>
+requires (!std::is_void_v<T>)
 using CoGenerator = CoRoutineT<CoRoutineType::Generator, T>;
+
 }
 
 #endif
